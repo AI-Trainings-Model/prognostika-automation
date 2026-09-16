512037e09d9bd020460d757517f743292df1c80e
korovinnn <124692541+korovinnn@users.noreply.github.com>
2026-09-16 23:33:58 +0200

    Add Android widget: app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.weatherwithyou.widget"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.weatherwithyou.widget"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
